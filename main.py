866fe0c461d85e21b9c7ab5b238048f14508e91c
CadrielZR <93616962+CadrielZR@users.noreply.github.com>
2022-03-30 14:15:37 -0300

    playerTemp added

--- main.py
+++ main.py
@@ -2,18 +2,27 @@ import pygame
 
 pygame.init()
 
+#Screen settings
 screen = pygame.display.set_mode((800,600))
-
 pygame.display.set_caption("Dungeoneer")
 icon = pygame.image.load('icon.png')
 pygame.display.set_icon(icon)
 
-running = True
+#Player
+playerImg = pygame.image.load('playerTemp.gif')
+playerX = 350
+playerY = 300
 
+def player():
+    screen.blit(playerImg,(playerX,playerY))
+
+#Game Loop
+running = True
 while running:
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0,0,0))
+    player()
     pygame.display.update()
