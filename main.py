aec40c8b6ca966a41d92a8cc8d808d47424d9907
CadrielZR <93616962+CadrielZR@users.noreply.github.com>
2022-04-06 17:18:23 -0300

    Player movement added

--- main.py
+++ main.py
@@ -12,17 +12,40 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('playerTemp.gif')
 playerX = 350
 playerY = 300
+playerX_change = 0
+playerY_change = 0
 
-def player():
-    screen.blit(playerImg,(playerX,playerY))
+def player(x,y):
+    screen.blit(playerImg,(x,y))
 
 #Game Loop
 running = True
 while running:
     screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
+                playerY_change = 0.2
+            if event.key == pygame.K_w:
+                playerY_change = -0.2
+
+            if event.key == pygame.K_d:
+                playerX_change = 0.2
+            if event.key == pygame.K_a:
+                playerX_change = -0.2
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_s or event.key == pygame.K_w:
+                playerY_change = 0
+            if event.key == pygame.K_a or event.key == pygame.K_d:
+                playerX_change = 0
+
+
+    playerY += playerY_change
+    playerX += playerX_change
+    player(playerX,playerY)
     pygame.display.update()
